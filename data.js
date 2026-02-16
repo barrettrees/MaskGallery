36de4be676f447981d27de99fc67f6c7360b1ad9
Barrett <barrettrees@gmail.com>
2026-02-15 23:51:57 -0800

    changed column widths

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ var masks = [
 		poster: "frontpage_communitycenter.png",
 		obs_photo: "communitycenter_default.jpg",
 		obs_photo_mouseover: "communitycenter_mouseover.jpg",
-		credit: ["San Mateo California, Monday, January 26, 5:18pm"],
+		credit: ["San Mateo California, Monday, January 26, 5:18pm - Barrett Anderson"],
 		description: "<p>\“For the project, here's a photo from today of the Masonic lodge in San Mateo. \
 		It's for sale, and it's used as an event space. I've never been in, but sometimes it seems like it's hopping.”</p> \
 		- Barrett Anderson "
@@ -18,7 +18,7 @@ var masks = [
 		poster: "frontpage_mall.png",
 		obs_photo: "closingsaleimage_default.jpg",
 		obs_photo_mouseover: "closingsaleimage_mouseover.jpg",
-		credit: ["San Mateo California, Tuesday, 2:58pm"],
+		credit: ["San Mateo California, Tuesday, 2:58pm - Barrett Anderson"],
 		description: "<p>“Here are a few from the south end of B Street, where the closed stores are. \
 		Old, abandoned newspaper box, recently closed grocery store and toy store that shut down a while ago. \
 		Walk just one block little north and it's busy and lively, but this area is all relics. \
@@ -30,7 +30,7 @@ var masks = [
 		poster: "frontpage_pine.png",
 		obs_photo: "pineforestimage_default.jpg",
 		obs_photo_mouseover: "pineforestimage_mouseover.jpg",
-		credit: ["Florida, Monday, January 26, 4:09pm "],
+		credit: ["Florida, Monday, January 26, 4:09pm - Emily Green"],
 		description: "<p>\“Pine trees that I was trying to identify on a walk the other day\”</p> - Lisa Green"
 	}, {
 		name: "Robin",
@@ -39,7 +39,7 @@ var masks = [
 		poster: "frontpage_robin.png",
 		obs_photo: "robin_default.jpg",
 		obs_photo_mouseover: "robin_mouseover.jpg",
-		credit: ["Whiskeytown California, Monday, January 26, 4:40pm"],
+		credit: ["Whiskeytown California, Monday, January 26, 4:40pm - Emily Green"],
 		description: "<p>“I\’ll raise you THREE observations: \
 		<br>1. Rosemary has started flowering \
 		<br>2. The robins are here for their evening visit\
@@ -51,7 +51,7 @@ var masks = [
 		poster: "frontpage_charizard.png",
 		obs_photo: "charizardcard_default.jpg",
 		obs_photo_mouseover: "charizardcard_mouseover.jpg",
-		credit: ["Florida, Monday, January 26, 4:06pm"],
+		credit: ["Florida, Monday, January 26, 4:06pm - Lisa Green"],
 		description: "<p>\“I didn’t notice this outside, but it was on my Facebook recently. \
 		It’s a teacher that I worked with who’s got two kids. I can’t believe that Pokémon still lives, \
 		in the same realm that it lived in when you were the same age!\”</p> - Lisa Green</p>"
@@ -63,7 +63,7 @@ var masks = [
 		poster: "frontpage_quail.png",
 		obs_photo: "quail_default.jpg",
 		obs_photo_mouseover: "quail_mouseover.jpg",
-		credit: ["Whiskeytown, California, Wednesday, 10:31am"],
+		credit: ["Whiskeytown, California, Wednesday, 10:31am - Emily Green"],
 		description: "<p>\"My observation today is that the brave female quail showed up alone again looking for seed,\
 		 ran right by me like where\’s the stuff ?? \<br>So I put some out for her lol\
 		 <br>Now she’s calling others over\"</p> - Emily Green"
